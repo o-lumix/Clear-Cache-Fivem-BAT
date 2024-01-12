898a8a1979f0e86937b19b072c3ed2e11b43fa97
Lumix <lumixofficiel@gmail.com>
2024-01-12 22:33:41 +0100

    Add files via upload

---
+++ Delete Cache FiveM.bat
@@ -0,0 +1,4 @@
+rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\cache /s /q
+rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\nui-storage /s /q
+rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache /s /q
+rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv /s /q
