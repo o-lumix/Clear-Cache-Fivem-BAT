3f5c6d11dc8067636eceed5d6308eb0251b484d4
Lumix <bylumixxx@gmail.com>
2024-10-10 17:24:50 +0200

    V3

--- Delete Cache FiveM.bat
+++ Delete Cache FiveM.bat
@@ -1,15 +1,31 @@
 @echo off
-echo Clear Cache FiveM by Lumix
+
+
+echo =====================================================
+echo          Clear Cache FiveM by Lumix
+echo =====================================================
 echo.
-echo Appuyez sur 1 pour lancer le nettoyage ou tout autre chiffre pour fermer le programme
+
+echo Appuyez sur [1] pour lancer le nettoyage ou tout autre chiffre pour quitter.
+echo.
 set /p choice="Votre choix: "
+
 if %choice%==1 (
+    echo.
+    echo Nettoyage en cours...
+    timeout /t 2 >nul
     rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\cache /s /q
     rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\nui-storage /s /q
     rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache /s /q
     rmdir %userprofile%\AppData\Local\FiveM\FiveM.app\data\server-cache-priv /s /q
-    echo Votre cache est vider
+    echo.
+    echo Votre cache est maintenant vide !
 ) else (
+    echo Fermeture du programme...
+    timeout /t 2 >nul
     exit
 )
+
+
+echo.
 pause
